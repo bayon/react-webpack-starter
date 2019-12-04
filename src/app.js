4cd98e71c514468654f844d40ccf66928ceddf22
Bayon Forte <bayon@forteworks.com>
2019-12-04 09:36:01 -0500

    log out to check

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import React from "react";
 import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
+
   render() {
+    console.log("react or not react.");
     const { name } = this.props;
     return <h1>Hello {name}</h1>;
   }

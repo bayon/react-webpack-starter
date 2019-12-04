c9bece307009b8fb67b84d9ad5580b6ab376bb79
Bayon Forte <bayon@forteworks.com>
2019-12-04 10:10:41 -0500

    change the doc id in react index

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { hot } from 'react-hot-loader/root';
 class App extends React.Component {
 
   render() {
-    console.log("react or not react.");
+     
     const { name } = this.props;
     return <h1>Hello {name}</h1>;
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+//document.getElementById('challenge-node')
+//var mountNode = document.getElementById("app");
+//ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(document.getElementById('app'));
